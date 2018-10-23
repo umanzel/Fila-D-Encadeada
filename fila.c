75a3575c2fc9f9923fadea9809be4332bf8bb793
umanzel <38693088+umanzel@users.noreply.github.com>
2018-10-23 10:31:07 -0300

    Add files via upload

--- fila.c
+++ fila.c
@@ -52,7 +52,7 @@ int main(int argc, char *argv[]) {
 
                case 2:
                Clear();
-               printf("%s", "Valor: ");
+               printf("%s", "Senha: ");
                scanf("%d", &aux);
                item.Chave = aux;
                Enfileira(item, &fila);
@@ -65,7 +65,7 @@ int main(int argc, char *argv[]) {
 
                case 4:
                Clear();
-               printf("%s", "Valor prioritario: ");
+               printf("%s", "Senha Prioritaria: ");
                scanf("%d", &aux);
                chave = aux;
                AumentaPrioridade(&fila, chave);
@@ -147,17 +147,48 @@ void Imprime(TipoFila Fila, int direcao) {
 }
 
 void AumentaPrioridade(TipoFila *Fila, TipoChave Chave) {
-     TipoApontador Aux;
-     TipoItem item;
-     item.Chave = Chave;
+     TipoApontador q, retorno, Aux;
+     TipoItem Item;
      Aux = Fila -> Frente;
+
+     //Localiza Ponteiro
+  	if(Vazia(*Fila)){
+  		printf("Não foi possivel localizar essa Chave na Fila.\n");
+    		return;
+	}
+	q = Fila -> Frente -> Prox;
+	if(q -> Item.Chave == Chave){
+		retorno = Fila -> Frente;
+	}
+	else{
+		do{
+			retorno = q;
+			q = q -> Prox;
+		}
+		while(q -> Item.Chave != Chave);
+	}
+
+     //Tira da Fila
+     if(Vazia(*Fila) || retorno == NULL || retorno -> Prox == NULL){
+		printf("Não foi possivel retirar esse item da lista.\n");
+		return;
+	}
+	q = retorno -> Prox;
+	Item = q -> Item;
+	retorno -> Prox = q -> Prox;
+	if(retorno -> Prox == NULL){
+		Fila -> Tras = retorno;
+	}
+	free(q);
+
+     //Coloca no começo
      if(Vazia(*Fila)){
           printf("Erro fila esta vazia\n");
           return;
      }
      Fila -> Frente -> Ante = (TipoApontador) malloc(sizeof(TipoCelula));
      Fila -> Frente = Fila -> Frente -> Ante;
-     Fila -> Frente -> Item = item;
+     Fila -> Frente -> Item = Item;
      Fila -> Frente -> Ante = NULL;
      Fila -> Frente -> Prox = Aux;
 }
